4d0681fdb3afdddf850c52f18afa5a5b1051595c
smeans <smeans@gmail.com>
2015-08-03 18:09:03 -0400

    dimension label refactoring

--- js/xtab.js
+++ js/xtab.js
@@ -24,14 +24,13 @@
     }
 
     function dragStart(e) {
-      console.log('dragstart: ' + e);
       dragging = e.target;
     }
 
     function canDrop(e) {
       var p = $(dragging).parent();
 
-      return (($(p).hasClass('xtab-dims-list') && $(e.target).closest('.xtab-dims').length)
+      return (($(p).hasClass('xtab-dims-list') && $(e.target).closest('.xtab-dims, .xtab-filters').length)
           || ($(p).hasClass('xtab-vals-list') && $(e.target).closest('.xtab-vals').length));
     }
 
@@ -48,7 +47,18 @@
       if ($(p).hasClass('xtab-dims-list') && $(e.target).closest('.xtab-filters').length) {
         xtab.addFilter($(dragging).text());
       }
-      //    || ($(p).hasClass('xtab-vals-list') && $(e.target).closest('.xtab-vals').length));
+
+      if ($(p).hasClass('xtab-dims-list') && $(e.target).closest('.xtab-dims.xtab-h').length) {
+        xtab.addHDim($(dragging).text());
+      }
+
+      if ($(p).hasClass('xtab-dims-list') && $(e.target).closest('.xtab-dims.xtab-v').length) {
+        xtab.addVDim($(dragging).text());
+      }
+
+      if ($(p).hasClass('xtab-vals-list') && $(e.target).closest('.xtab-vals').length) {
+        xtab.setValue($(dragging).text());
+      }
 
 
       dragging = null;
@@ -63,12 +73,101 @@
       }
     }
 
+    function refreshData(e) {
+      getXtab(e.target).refreshData();
+    }
+
+    function resetXtab(e) {
+      var xtab = getXtab(e.target);
+
+      xtab.reset();
+      xtab.refreshData();
+    }
+
+    function buildDLL(xtab, data, dims, level, filter) {
+      if (level >= dims.length) {
+        return null;
+      }
+
+      var dl = xtab.getDimValues(dims[level], data);
+      dl.sort();
+      var html = '';
+
+      $.each(dl, function () {
+        html += '<li>' + this
+
+
+        filter[dims[level]] = this;
+        var cl = buildDLL(xtab, data.filterByObject(filter), dims, level+1, filter);
+        delete filter[dims[level]];
+
+        if (cl) {
+          html += '<ul>' + cl + "</ul>";
+        }
+        html += '</li>'
+      });
+
+      return html;
+    }
+
+    function buildDimLabels(xtab, data, dims) {
+      var dlr = $('<ul></ul>');
+
+      var dll = buildDLL(xtab, data, dims, 0, {});
+      if (dll) {
+        $(dlr).append(dll);
+      }
+
+      return dlr;
+    }
+
+    function buildDTW(xtab, data, dims, level, filter) {
+      if (level >= dims.length) {
+        return null;
+      }
+
+      var dta = [];
+      var dl = xtab.getDimValues(dims[level], data);
+      dl.sort();
+
+      $.each(dl, function () {
+        filter[dims[level]] = this;
+        var cl = buildDTW(xtab, data.filterByObject(filter), dims, level+1, filter);
+        delete filter[dims[level]];
+        dta.push({name:this, children:cl});
+      });
+
+      return dta;
+    }
+
+    function buildDimTree(xtab, data, dims) {
+      return buildDTW(xtab, data, dims, 0, {});
+    }
+
+    Array.prototype.filterByObject = function (o) {
+      return this.filter(function (el) {
+        for (var k in o) {
+          if (o.hasOwnProperty(k) && k in el) {
+            if (Array.isArray(o[k])) {
+              if (o[k].indexOf(el[k]) < 0) {
+                return false;
+              }
+            } else if (o[k] != el[k]) {
+              return false;
+            }
+          }
+        }
+
+        return true;
+      });
+    }
+
     XTab.prototype = {
         init: function() {
           var xt = this;
           $(this.element).append('<div class="xtab-dims-list"><label>dimensions</label></div><div class="xtab-vals-list"><label>values</label></div>');
-          $(this.element).append('<div class="xtab-filters xtab-dims"><label>filters</label></div>');
-          $(this.element).append('<table class="xtab-data"><tbody><tr><td class="xtab-corner">&nbsp;</td><td class="xtab-dims xtab-h">&nbsp;</td></tr><tr><td class="xtab-dims xtab-v">&nbsp;</td><td class="xtab-vals">&nbsp;</td></tr></tbody></table>');
+          $(this.element).append('<div class="xtab-filters"><label>filters</label></div>');
+          $(this.element).append('<table class="xtab-data"><tbody><tr><td class="xtab-corner"><button name="xtabReset">reset</button></td><td class="xtab-dims xtab-h">&nbsp;</td></tr><tr><td class="xtab-dims xtab-v">&nbsp;</td><td class="xtab-vals">&nbsp;</td></tr></tbody></table>');
 
           $.each(this.options.dimensions, function () {
             $('.xtab-dims-list', xt.element).append('<div draggable="true">' + this + '</div>');
@@ -80,21 +179,38 @@
           $(this.element).on('dragstart', dragStart);
           $(this.element).on('dragover', dragOver);
           $(this.element).on('drop', drop);
+          $('button[name=xtabReset]', this.element).click(resetXtab);
 
           $('.xtab-filters', this.element).click(removeFilter);
+          $('.xtab-filters', this.element).change(refreshData);
 
+          this.reset();
+        },
+
+        reset: function () {
           this._filters = [];
           this._h_dims = [];
           this._v_dims = [];
-          this._dimcache = {};
+
+          $('.xtab-filters div', this.element).remove();
+          this.refreshData();
         },
 
-        getDimValues: function(dim) {
-          if (dim in this._dimcache) {
-            return this._dimcache[dim];
-          }
-
+        getFilteredData: function() {
           var data = this.options.data;
+          var fvd = {};
+          $('.xtab-filters div', this.element).each(function () {
+            var dim = $(this).attr('name');
+            fvd[dim] = $('.selstrip', this).selStrip('values');
+          });
+
+          return data.filterByObject(fvd);
+        },
+
+        getDimValues: function (dim, data) {
+          if (!data) {
+            data = this.getFilteredData();
+          }
           var dl = [];
 
           for (var i = 0; i < data.length; i++) {
@@ -103,7 +219,16 @@
             }
           }
 
-          return this._dimcache[dim] = dl;
+          return dl;
+        },
+
+        getFilteredDimValues: function (dim) {
+          var fss = $('.xtab-filters div[name=' + dim +'] .selstrip');
+          if (fss.length > 0) {
+            return $(fss).selStrip('values');
+          }
+
+          return this.getDimValues(dim);
         },
 
         addFilter: function (dim) {
@@ -115,8 +240,10 @@
           var fd = $('<div name="' + dim + '"><label><i class="fa fa-times-circle"></i>&nbsp;' + dim + '</label><div class="selstrip"></div>');
           $('.selstrip', fd).selStrip({values:this.getDimValues(dim)});
 
-          $('.xtab-filters div').selStrip('readonly', true);
+          $('.xtab-filters div', this.element).selStrip('readonly', true);
           $('.xtab-filters', this.element).append(fd);
+
+          this.refreshData();
         },
 
         removeFilter: function (dim) {
@@ -126,6 +253,51 @@
             this._filters.splice(i, 1);
             $('.xtab-filters div[name=' + dim + ']', this.element).remove();
           }
+
+          this.refreshData();
+        },
+
+        isActiveDim: function (dim) {
+          return this._h_dims.indexOf(dim) > -1 || this._v_dims.indexOf(dim) > -1;
+        },
+
+        addHDim: function (dim) {
+          if (this.isActiveDim(dim)) {
+            return;
+          }
+
+          this._h_dims.push(dim);
+
+          this.refreshData();
+        },
+
+        addVDim: function (dim) {
+          if (this.isActiveDim(dim)) {
+            return;
+          }
+
+          this._v_dims.push(dim);
+
+          this.refreshData();
+        },
+
+        setValue: function (val) {
+          this._value = val;
+
+          this.refreshData();
+        },
+
+        refreshData: function () {
+          var data = this.getFilteredData();
+
+          var hdt = buildDimTree(this, data, this._h_dims);
+          console.log(hdt);
+
+          var hdl = buildDimLabels(this, data, this._h_dims);
+          $('.xtab-dims.xtab-h', this.element).html(hdl);
+
+          var vdl = buildDimLabels(this, data, this._v_dims);
+          $('.xtab-dims.xtab-v', this.element).html(vdl);
         }
     };
 
